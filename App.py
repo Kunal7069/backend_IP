e4a015fbba6bdb2221f8305c1ff1059b19d80331
Kunal7069 <jaink7069@gmail.com>
2024-09-03 20:26:07 +0530

    DONE

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import boto3
 import time
-
+import os
 app = Flask(__name__)
 CORS(app)  
 
@@ -83,4 +83,5 @@ def home():
     return jsonify({'status': 'HOME PAGE'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
