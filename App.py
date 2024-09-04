e1bb91c7f52c97156d8284c3202e993074274833
Kunal7069 <jaink7069@gmail.com>
2024-09-04 13:17:45 +0530

    DONE

--- App.py
+++ App.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import boto3
-import time
 import os
 app = Flask(__name__)
 CORS(app)  
@@ -10,13 +9,14 @@ stop_allocation = False
 
 
 desired_ips = [
-    '43.204.22','43.204.25', '43.204.24', '43.204.23', '43.204.28', '43.204.30',  
+    '44.219.200','43.204.22','43.204.25', '43.204.24', '43.204.23', '43.204.28', '43.204.30',  
     '43.205.232', '43.205.113', '43.205.207', '43.205.178', '43.205.126', '43.205.124', '43.205.113', 
     '43.205.208', '43.205.191', '43.205.94', '43.205.228', '43.205.210', '43.205.191', '43.205.120', '43.205.238', '43.205.207', '43.205.110', '43.205.121', 
     '43.205.107', '43.205.123', '43.205.92', '43.205.213', '43.205.217', '43.205.130', '43.205.220', '43.205.194', '43.205.178', 
     '43.205.210', '43.205.111', '43.205.215', '43.205.253', '43.205.49', '43.205.52', '43.205.153', '43.205.50', 
     '43.205.94', '43.205.208', '43.205.217', '43.205.142', '43.205.146'
 ]
+
 
 def get_first_three_parts(ip_address):
     print('.'.join(ip_address.split('.')[:3]))
@@ -27,31 +27,31 @@ def create_elastic_ip(ec2_client):
     allocated_ips = []
 
     try:
-        while not stop_allocation and desired_ips:
-            for desired_ip in desired_ips:
-                if stop_allocation:
-                    break
+        while not stop_allocation:
 
-                try:
-                    response = ec2_client.allocate_address(Domain='vpc')
-                    ip_address = response['PublicIp']
-                    allocation_id = response['AllocationId']
+            try:
+                response = ec2_client.allocate_address(Domain='vpc')
+                ip_address = response['PublicIp']
+                allocation_id = response['AllocationId']
+                release=0
+                print(f"Allocated IP address: {ip_address}")
 
-                    print(f"Allocated IP address: {ip_address}")
-
-                    # Compare the first three parts of the IP addresses
+                for desired_ip in desired_ips:
+                    print("DESIRED IP ADDRESS",desired_ip )
+                    
                     if get_first_three_parts(ip_address) == get_first_three_parts(desired_ip):
                         allocated_ips.append({'ip_address': ip_address, 'status': 'success'})
-                        desired_ips.remove(desired_ip)  # Remove allocated IP from list
+                        desired_ips.remove(desired_ip) 
+                        release=1 
                         print(f"Desired IP address {desired_ip} has been allocated!")
-                    else:
-                        ec2_client.release_address(AllocationId=allocation_id)
-                        print(f"Released IP address: {ip_address}")
+                    
+                if release==0:
+                    ec2_client.release_address(AllocationId=allocation_id)
+                    print(f"Released IP address: {ip_address}")
+            except Exception as e:
+                return {'status': 'error', 'message': str(e)}
 
-                except Exception as e:
-                    return {'status': 'error', 'message': str(e)}
-
-                time.sleep(2)
+                
 
         return {'status': 'completed' if not desired_ips else 'stopped', 'allocated_ips': allocated_ips}
     except Exception as e:
